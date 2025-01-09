318a19b8367aa80a388b0ea033a7242b68392769
izipay-pe <103148193+izipay-pe@users.noreply.github.com>
2025-01-09 09:44:52 -0500

    Update key.py

--- key.py
+++ key.py
@@ -1,6 +1,10 @@
 credentials = {
-    "USERNAME": "CHANGE_ME_USER_ID",
-    "PASSWORD": "CHANGE_ME_PASSWORD",
-    "PUBLIC_KEY": "CHANGE_ME_PUBLIC_KEY",
-    "HMACSHA256": "CHANGE_ME_HMAC_SHA_256"
+    # Identificador de su tienda
+    "USERNAME": "~ CHANGE_ME_USER_ID ~",
+    # Clave de Test o Producción
+    "PASSWORD": "~ CHANGE_ME_PASSWORD ~",
+    # Clave Pública de Test o Producción
+    "PUBLIC_KEY": "~ CHANGE_ME_PUBLIC_KEY ~",
+    # Clave HMAC-SHA-256 de Test o Producción
+    "HMACSHA256": "~ CHANGE_ME_HMAC_SHA_256 ~"
 }
